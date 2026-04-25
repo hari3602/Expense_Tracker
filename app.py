35d4bbafdcaafeb001c8d04602e6136ffb99f320
Hari3602 <hari.v3602@gmail.com>
2026-04-25 21:28:28 +0530

    refactor

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from Transaction import Transaction
 from Financeservice import FinanceService
 from storage import Storage
@@ -11,6 +12,11 @@ class FinanceTracker:
 
     def add_cli(self):
         date = input("Enter date: ")
+        try:
+            datetime.strptime(date, "%d/%m/%Y")
+        except ValueError:
+            print("Invalid date format. Please use dd/mm/yyyy.")
+            return
         t_type = input("Type (income/expense): ").strip().lower()
         if t_type not in ["income", "expense"]:
             print("Invalid type")
@@ -32,25 +38,26 @@ class FinanceTracker:
             return
         self.service.delete_transaction(txn_index)
 
+    def print_transactions(self, data):
+        """Helper method to print transaction data."""
+        print("Index \t Date \t Transction type \t Category \t Amount \n")
+        print("-" * 60)
+        for txn in data:
+            if isinstance(txn, tuple):  # For filtered transactions (income/expense)
+                index, transaction = txn
+            else:  # For all transactions
+                index, transaction = data.index(txn) + 1, txn
+            sign = "+" if transaction.type == "income" else "-"
+            print(f"{index}\t{transaction.date}\t{transaction.type}\t{transaction.category}\t{sign}{transaction.amount}")
+
     def list_cli(self):
         list_filter = input("List of (Income/Expense/all):").strip().lower()
         if list_filter == "all":
             data = self.service.list_transactions()
-            print("Index \t Date \t Transction type \t Category \t Amount \n")
-            print("-"*60)
-            for index, txn in enumerate(data, start=1):
-                sign = "+" if txn.type == "income" else "-"
-                print(f"{index}\t{txn.date}\t{txn.type}\t{txn.category}\t{sign}{txn.amount}")
-        elif list_filter == "income" or "expense":
+            self.print_transactions(data)
+        elif list_filter in ["income", "expense"]:
             data = self.service.list_transaction_by_type(list_filter)
-            print("Index \t Date \t Transction type \t Category \t Amount \n")
-            print("-"*60)
-            for txn in data:
-                sign = "+" if txn[1].type == "income" else "-"
-                print(f"{txn[0]}\t{txn[1].date}\t{txn[1].type}\t{txn[1].category}\t{sign}{txn[1].amount}")
-
-        elif list_filter == "expense":
-            pass
+            self.print_transactions(data)
         else:
             print("Enter valid input")
 
